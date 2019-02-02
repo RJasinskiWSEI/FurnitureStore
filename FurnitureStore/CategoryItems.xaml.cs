7c88de19776e8eaca6129ba34d5a3b5de48b9561
Rafal Jasinski <hinofe@ace-mail.net>
2019-02-02 20:45:58 +0100

    Navigation base.
    Created base for paged navigation (frame with content).

--- FurnitureStore/CategoryItems.xaml.cs
+++ FurnitureStore/CategoryItems.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FurnitureStore.Windows
+namespace FurnitureStore
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CategoryItems.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CategoryItems : Page
     {
-        public MainWindow()
+        public CategoryItems()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Main.Content = new CategoryItems();
         }
     }
 }
