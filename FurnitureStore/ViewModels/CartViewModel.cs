d3535927482d1a798b8b7688f1d407c61bf6f3ea
Rafal Jasinski <hinofe@ace-mail.net>
2019-02-06 23:09:11 +0100

    Cart view.

--- FurnitureStore/ViewModels/CartViewModel.cs
+++ FurnitureStore/ViewModels/CartViewModel.cs
@@ -1,15 +1,49 @@
-﻿using FurnitureStore.ViewModels.Base;
+﻿using FurnitureStore.Commands;
+using FurnitureStore.Infrastructure.Services.Cart;
+using FurnitureStore.ViewModels.Base;
+using FurnitureStore.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
+using static FurnitureStore.Infrastructure.Services.Cart.CartService;
 
 namespace FurnitureStore.ViewModels
 {
     public class CartViewModel : ViewModelBase
     {
+        private ObservableCollection<CartItem> _cartItems;
+        private readonly ICartService _cartService;
+
+        public ObservableCollection<CartItem> CartItems
+        {
+            get => _cartItems;
+            set => SetValue(ref _cartItems, value);
+        }
+
+        public CartViewModel(ICartService cartService)
+        {
+            _cartItems = new ObservableCollection<CartItem>();
+            _cartService = cartService;
+        }
+
+        public ICommand ClearShoppingCart => new Command(async () =>
+        {
+            _cartService.ClearCart();
+            await Initialize();
+        });
+
+        public override Task<bool> Initialize()
+        {
+            CartItems = _cartService.GetCartItems().ToObservableCollection();
+
+            return Task.FromResult(true);
+        }
+
         public override Page GetPage()
         {
             return new CartView();
--- FurnitureStore/Views/CartView.xaml.cs
+++ FurnitureStore/Views/CartView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FurnitureStore
+namespace FurnitureStore.Views
 {
     /// <summary>
     /// Interaction logic for CartView.xaml
